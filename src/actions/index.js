0d11757814334f4d88891e914e0a1a8e7f5193d5
renevanderark <r.van.der.ark@gmail.com>
2016-02-09 12:27:49 +0100

    moves skeleton logic to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,38 +2,7 @@ import store from "../store";
 import fieldDefinitions from "../static/field-definitions";
 
 
-const dataTypes = {
-	names: [],
-	multiselect: [],
-	links: [],
-	keyword: [],
-	text: "",
-	string: "",
-	select: "",
-	datable: ""
-};
-
-const initialDataForType = (fieldDef) =>
-	fieldDef.type === "relation" ? {} : dataTypes[fieldDef.type];
-
-const nameForType = (fieldDef) =>
-	fieldDef.type === "relation" || fieldDef.type === "keyword" ? "@relations" : fieldDef.name;
-
-const makeSkeleton = (fieldDefs) =>
-	fieldDefs
-		.map((fieldDef) => [nameForType(fieldDef), initialDataForType(fieldDef)])
-		.reduce((obj, cur) => {
-			obj[cur[0]] = cur[1];
-			return obj;
-		}, {});
-
-const getFieldDescription = (domain, dispatch) =>
-	dispatch({
-		type: "NEW_ENTITY",
-		domain: domain,
-		fieldDefinitions: fieldDefinitions[domain],
-		data: makeSkeleton(fieldDefinitions[domain])
-	});
+const getFieldDescription = (domain, dispatch) => dispatch({type: "NEW_ENTITY", domain: domain, fieldDefinitions: fieldDefinitions[domain]});
 
 export default {
 	onNew: (domain) => store.dispatch(redispatch => getFieldDescription(domain, redispatch))
--- src/reducers/entity.js
+++ src/reducers/entity.js
@@ -1,3 +1,29 @@
+const dataTypes = {
+	names: [],
+	multiselect: [],
+	links: [],
+	keyword: [],
+	text: "",
+	string: "",
+	select: "",
+	datable: ""
+};
+
+const initialDataForType = (fieldDef) =>
+	fieldDef.type === "relation" ? {} : dataTypes[fieldDef.type];
+
+const nameForType = (fieldDef) =>
+	fieldDef.type === "relation" || fieldDef.type === "keyword" ? "@relations" : fieldDef.name;
+
+const makeSkeleton = (fieldDefs) =>
+	fieldDefs
+		.map((fieldDef) => [nameForType(fieldDef), initialDataForType(fieldDef)])
+		.reduce((obj, cur) => {
+			obj[cur[0]] = cur[1];
+			return obj;
+		}, {});
+
+
 let initialState = {
 	data: null,
 	domain: null,
@@ -8,7 +34,7 @@ export default function(state=initialState, action) {
 	switch (action.type) {
 		case "NEW_ENTITY":
 			state = {...state, ...{
-				data: action.data,
+				data: makeSkeleton(action.fieldDefinitions),
 				domain: action.domain,
 				fieldDefinitions: action.fieldDefinitions
 			}};
