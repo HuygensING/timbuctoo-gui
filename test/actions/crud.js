746c7a2f0a45517474aa4bc6b259b833f7a9bb1a
renevanderark <r.van.der.ark@gmail.com>
2016-02-17 11:57:33 +0100

    fixes async test exception handling

--- test/actions/crud.js
+++ test/actions/crud.js
@@ -9,25 +9,35 @@ describe("crud", () => { //eslint-disable-line no-undef
 
 	it("should fetch an entity with GET invoke next with response data", (done) => { //eslint-disable-line no-undef
 		sinon.stub(server, "performXhr", (options, accept, reject, operation) => {
-			expect(options).toEqual({
-				url: "/loc",
-				headers: {
-					"Accept": "application/json"
-				},
-				method: "GET"
-			});
-			expect(operation).toEqual("Fetch entity");
-			expect(reject()).toEqual("reject_GET");
-			accept(null, {body: JSON.stringify({data: "data"})});
+			try {
+				expect(options).toEqual({
+					url: "/loc",
+					headers: {
+						"Accept": "application/json"
+					},
+					method: "GET"
+				});
+				expect(operation).toEqual("Fetch entity");
+				expect(reject()).toEqual("reject_GET");
+				accept(null, {body: JSON.stringify({data: "data"})});
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
 		});
 
 		fetchEntity("/loc", (data) => {
+			try {
 				expect(data).toEqual({data: "data"});
+				sinon.assert.calledOnce(server.performXhr);
+				server.performXhr.restore();
 				done();
-			}, () => "reject_GET");
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
+		}, () => "reject_GET");
 
-		sinon.assert.calledOnce(server.performXhr);
-		server.performXhr.restore();
 	});
 
 	it("should update an entity with PUT", (done) => { //eslint-disable-line no-undef
@@ -37,27 +47,32 @@ describe("crud", () => { //eslint-disable-line no-undef
 		const domain = "dom";
 
 		sinon.stub(server, "performXhr", (options, accept, reject, operation) => {
-			expect(options).toEqual({
-				url: `/api/${config.apiVersion}/domain/${domain}s/${saveData._id}`,
-				headers: {
-					"Content-type": "application/json",
-					"Accept": "application/json",
-					"Authorization": token,
-					"VRE_ID": vreId
-				},
-				method: "PUT",
-				body: JSON.stringify(saveData)
-			});
-			expect(operation).toEqual(`Update ${domain}`);
-			expect(accept()).toEqual("accept_PUT");
-			expect(reject()).toEqual("reject_PUT");
-			done();
+			try {
+				expect(options).toEqual({
+					url: `/api/${config.apiVersion}/domain/${domain}s/${saveData._id}`,
+					headers: {
+						"Content-type": "application/json",
+						"Accept": "application/json",
+						"Authorization": token,
+						"VRE_ID": vreId
+					},
+					method: "PUT",
+					body: JSON.stringify(saveData)
+				});
+				expect(operation).toEqual(`Update ${domain}`);
+				expect(accept()).toEqual("accept_PUT");
+				expect(reject()).toEqual("reject_PUT");
+				sinon.assert.calledOnce(server.performXhr);
+				server.performXhr.restore();
+				done();
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
 		});
 
 		updateEntity(domain, saveData, token, vreId, () => "accept_PUT", () => "reject_PUT");
 
-		sinon.assert.calledOnce(server.performXhr);
-		server.performXhr.restore();
 	});
 
 
@@ -68,26 +83,32 @@ describe("crud", () => { //eslint-disable-line no-undef
 		const domain = "dom";
 
 		sinon.stub(server, "performXhr", (options, accept, reject, operation) => {
-			expect(options).toEqual({
-				url: `/api/${config.apiVersion}/domain/${domain}s/${entityId}`,
-				headers: {
-					"Content-type": "application/json",
-					"Accept": "application/json",
-					"Authorization": token,
-					"VRE_ID": vreId
-				},
-				method: "DELETE"
-			});
-			expect(operation).toEqual(`Delete ${domain}`);
-			expect(accept()).toEqual("accept_DELETE");
-			expect(reject()).toEqual("reject_DELETE");
-			done();
+			try {
+				expect(options).toEqual({
+					url: `/api/${config.apiVersion}/domain/${domain}s/${entityId}`,
+					headers: {
+						"Content-type": "application/json",
+						"Accept": "application/json",
+						"Authorization": token,
+						"VRE_ID": vreId
+					},
+					method: "DELETE"
+				});
+				expect(operation).toEqual(`Delete ${domain}`);
+				expect(accept()).toEqual("accept_DELETE");
+				expect(reject()).toEqual("reject_DELETE");
+				sinon.assert.calledOnce(server.performXhr);
+				server.performXhr.restore();
+				done();
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
 		});
 
 		deleteEntity(domain, entityId, token, vreId, () => "accept_DELETE", () => "reject_DELETE");
 
-		sinon.assert.calledOnce(server.performXhr);
-		server.performXhr.restore();
+
 	});
 
 
@@ -98,26 +119,32 @@ describe("crud", () => { //eslint-disable-line no-undef
 		const domain = "dom";
 
 		sinon.stub(server, "performXhr", (options, accept, reject, operation) => {
-			expect(options).toEqual({
-				url: `/api/${config.apiVersion}/domain/${domain}s`,
-				headers: {
-					"Content-type": "application/json",
-					"Accept": "application/json",
-					"Authorization": token,
-					"VRE_ID": vreId
-				},
-				method: "POST",
-				body: JSON.stringify(saveData)
-			});
-			expect(operation).toEqual(`Create new ${domain}`);
-			expect(accept()).toEqual("accept_POST");
-			expect(reject()).toEqual("reject_POST");
-			done();
+			try {
+				expect(options).toEqual({
+					url: `/api/${config.apiVersion}/domain/${domain}s`,
+					headers: {
+						"Content-type": "application/json",
+						"Accept": "application/json",
+						"Authorization": token,
+						"VRE_ID": vreId
+					},
+					method: "POST",
+					body: JSON.stringify(saveData)
+				});
+				expect(operation).toEqual(`Create new ${domain}`);
+				expect(accept()).toEqual("accept_POST");
+				expect(reject()).toEqual("reject_POST");
+				sinon.assert.calledOnce(server.performXhr);
+				server.performXhr.restore();
+				done();
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
 		});
 
 		saveNewEntity(domain, saveData, token, vreId, () => "accept_POST", () => "reject_POST");
 
-		sinon.assert.calledOnce(server.performXhr);
-		server.performXhr.restore();
+
 	});
 });
--- test/actions/v2.1/save-relations.js
+++ test/actions/v2.1/save-relations.js
@@ -34,39 +34,92 @@ describe("saveRelations v2.1", () => { //eslint-disable-line no-undef
 			const payload = JSON.parse(options.body);
 			const relType = payload["@type"];
 			counts[relType]++;
-			expect(options.method).toEqual("POST");
 
-			if(relType === "relTypeA") {
+			try {
+				expect(options.method).toEqual("POST");
+				if(relType === "relTypeA") {
+					expect(payload).toEqual({
+						"@type": "relTypeA",
+						"^sourceId": "entityID",
+						"^sourceType": "document",
+						"^targetId": `A_${counts[relType]}`,
+						"^targetType": "person",
+						"^typeId": "typeID",
+						"accepted": true
+					});
+				} else {
+					expect(payload).toEqual({
+						"@type": "relTypeB",
+						"^sourceId": "B_1",
+						"^sourceType": "document",
+						"^targetId": "entityID",
+						"^targetType": "person",
+						"^typeId": "typeID",
+						"accepted": true
+					});
+				}
+
+				accept();
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
+			}
+		});
+
+		saveRelations(data, relationData, fieldDefs, "TOKEN", "VREID", () => {
+			try {
+				sinon.assert.calledThrice(server.performXhr);
+				server.performXhr.restore();
+				done();
+			} catch (e) {
+				server.performXhr.restore();
+				done(e);
+			}
+		});
+	});
+
+	it("should delete rejected relations with PUT", (done) => { //eslint-disable-line no-undef
+		const data = {_id: "entityID", "@relations": {
+			"relNameA": [{accepted: true, id: "A_1", relationId: "REL_1"}]
+		}};
+		const relationData = {"relNameA": []};
+		const fieldDefs = [{name: "relNameA", relation: { type: "relTypeA", isInverseName: false, sourceType: "document", targetType: "person", typeId: "typeID"}}];
+
+
+		sinon.stub(server, "performXhr", (options, accept) => {
+			const payload = JSON.parse(options.body);
+
+			try {
+				expect(options.method).toEqual("PUT");
 				expect(payload).toEqual({
 					"@type": "relTypeA",
 					"^sourceId": "entityID",
 					"^sourceType": "document",
-					"^targetId": `A_${counts[relType]}`,
+					"^targetId": `A_1`,
 					"^targetType": "person",
 					"^typeId": "typeID",
-					"accepted": true
+					"_id": "REL_1",
+					"accepted": false
 				});
-			} else {
-				expect(payload).toEqual({
-					"@type": "relTypeB",
-					"^sourceId": "B_1",
-					"^sourceType": "document",
-					"^targetId": "entityID",
-					"^targetType": "person",
-					"^typeId": "typeID",
-					"accepted": true
-				});
+				accept();
+			} catch(e) {
+				server.performXhr.restore();
+				done(e);
 			}
-
-			accept();
 		});
 
 		saveRelations(data, relationData, fieldDefs, "TOKEN", "VREID", () => {
-			sinon.assert.calledThrice(server.performXhr);
-			server.performXhr.restore();
-			done();
+			try {
+				sinon.assert.calledOnce(server.performXhr);
+				server.performXhr.restore();
+				done();
+			} catch (e) {
+				server.performXhr.restore();
+				done(e);
+			}
 		});
 	});
+
 
 	it("should handle server exceptions", (done) => { //eslint-disable-line no-undef
 		const data = {_id: "entityID", "@relations": {}};
@@ -83,74 +136,3 @@ describe("saveRelations v2.1", () => { //eslint-disable-line no-undef
 		});
 	});
 });
-
-/*
-import { saveNewEntity, updateEntity } from "../crud";
-
-const saveRelationsV21 = (data, relationData, fieldDefs, token, vreId, next) => {
-	// Returns the domain based on the fieldDefinitions and the relation key (i.e. "hasBirthPlace")
-	const makeRelationArgs = (relation, key, accepted = true, id = null, rev = null) => {
-		const fieldDef = fieldDefs.find((def) => def.name === key);
-		const relationSaveData = {
-			"@type": fieldDef.relation.type,
-			"^sourceId": fieldDef.relation.isInverseName ? relation.id : data._id,
-			"^sourceType": fieldDef.relation.isInverseName ? fieldDef.relation.targetType : fieldDef.relation.sourceType, // FIXME?
-			"^targetId": fieldDef.relation.isInverseName ? data._id : relation.id,
-			"^targetType": fieldDef.relation.isInverseName ? fieldDef.relation.sourceType : fieldDef.relation.targetType,  // FIXME?
-			"^typeId": fieldDef.relation.typeId,
-			accepted: accepted
-		};
-		if(id) { relationSaveData._id = id; }
-		if(rev) { relationSaveData["^rev"] = rev; }
-		return [
-			fieldDef.relation.type, // domain
-			relationSaveData
-		];
-	};
-
-	// Constructs an array of arguments for saving new relations:
-	// [
-	//   ["wwrelations", { ... }],
-	//   ["wwrelations", { ... }],
-	// ]
-	const newRelations = Object.keys(relationData).map((key) =>
-		relationData[key]
-			// Filters out all relations which are not already in data["@relations"]
-			.filter((relation) => (data["@relations"][key] || []).map((origRelation) => origRelation.id).indexOf(relation.id) < 0)
-			// Make argument array for new relations: ["wwrelations", { ... }]
-			.map((relation) => makeRelationArgs(relation, key))
-		// Flatten nested arrays
-		).reduce((a, b) => a.concat(b), []);
-
-
-	// Reactivate previously added relations using PUT which were 'deleted' after using PUT
-	const reAddRelations = Object.keys(relationData).map((key) =>
-		(data["@relations"][key] || [])
-			.filter((origRelation) => origRelation.accepted === false)
-			.filter((origRelation) => (relationData[key] || []).filter((relation) => relation.accepted).map((relation) => relation.id).indexOf(origRelation.id) > -1)
-			.map((origRelation) => makeRelationArgs(origRelation, key, true, origRelation.relationId, origRelation.rev))
-		).reduce((a, b) => a.concat(b), []);
-
-	// Deactivate previously added relations using PUT
-	const deleteRelations = Object.keys(data["@relations"]).map((key) =>
-		data["@relations"][key]
-			.filter((origRelation) => origRelation.accepted)
-			.filter((origRelation) => (relationData[key] || []).map((relation) => relation.id).indexOf(origRelation.id) < 0)
-			.map((origRelation) => makeRelationArgs(origRelation, key, false, origRelation.relationId, origRelation.rev))
-		).reduce((a, b) => a.concat(b), []);
-
-	// Combines saveNewEntity and deleteEntity instructions into promises
-	const promises = newRelations
-		// Map newRelations to promised invocations of saveNewEntity
-		.map((args) => new Promise((resolve) => saveNewEntity(...args, token, vreId, resolve) ))
-		// Map readdRelations to promised invocations of updateEntity
-		.concat(reAddRelations.map((args) => new Promise((resolve) => updateEntity(...args, token, vreId, resolve))))
-		// Map deleteRelations to promised invocations of updateEntity
-		.concat(deleteRelations.map((args) => new Promise((resolve) => updateEntity(...args, token, vreId, resolve))));
-
-	// Invoke all CRUD operations for the relations
-	Promise.all(promises).then(next);
-};
-
-export default saveRelationsV21;
-*/
