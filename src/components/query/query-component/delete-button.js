f248b89ce3b19934db56abd654caead61460fe38
renevanderark <r.van.der.ark@gmail.com>
2016-02-29 13:21:07 +0100

    adds red hover fill to delete button

--- src/components/query/query-component/delete-button.js
+++ src/components/query/query-component/delete-button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 let deleteButton = (props) => (
-	<g onClick={props.onSelect} transform={`translate(${props.translate || "-20 -20"})`} >
+	<g className="delete-button" onClick={props.onSelect} transform={`translate(${props.translate || "-20 -20"})`} >
 		<circle r="8" />
 		<line stroke="white" strokeWidth="1" x1="-3" x2="3" y1="-3" y2="3" />
 		<line stroke="white" strokeWidth="1" x1="-3" x2="3" y1="3" y2="-3" />
