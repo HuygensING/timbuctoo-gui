8565ba5c897ff7bf216fbe529b23463d60720678
renevanderark <r.van.der.ark@gmail.com>
2016-03-02 15:54:37 +0100

    implements queries reducer test for SET_QUERY_FIELD_VALUE

--- test/reducers/queries.js
+++ test/reducers/queries.js
@@ -1,15 +1,67 @@
 import expect from "expect";
+import sinon from "sinon";
+import clone from "clone-deep";
+
+import server from "../../src/actions/server";
 import queriesReducer from "../../src/reducers/queries";
 
+const sampleQuery = {
+	domain: "wwperson",
+	deleted: false,
+	pathToQuerySelection: ["entity", "and"],
+
+	entity: {
+		domain: "wwperson",
+		and: [
+			{type: "property", name: "gender", value: "FEMALE"}
+		]
+	}
+};
+
 describe("queries reducer", () => { //eslint-disable-line no-undef
+
+	before(() => { //eslint-disable-line no-undef
+		sinon.stub(server, "fastXhr");
+	});
+
+	after(() => { //eslint-disable-line no-undef
+		server.fastXhr.restore();
+	});
 
 	it("should SELECT_QUERY"); //eslint-disable-line no-undef
 
 	it("should SET_QUERY_PATH"); //eslint-disable-line no-undef
 
-	it("should SET_QUERY_FIELD_VALUE"); //eslint-disable-line no-undef
+	it("should immutably SET_QUERY_FIELD_VALUE", () => {  //eslint-disable-line no-undef
+		const queries = [{}, sampleQuery];
+		const beforeState = { currentQuery: 1, queries: queries };
+		const expectedQuery = clone(sampleQuery);
 
-	it("should DELETE_QUERY"); //eslint-disable-line no-undef
+		expectedQuery.entity.and[0].value = "MALE";
+
+		const expectedState = {
+			currentQuery: 1,
+			queries: [
+				{},
+				expectedQuery
+			],
+			resultCount: "",
+			resultCountPending: true,
+			resultsPending: true
+		};
+
+		const action = {
+			type: "SET_QUERY_FIELD_VALUE",
+			fieldPath: [0, "value"],
+			value: "MALE"
+		};
+
+		const actual = queriesReducer(beforeState, action);
+
+		expect(actual).toEqual(expectedState);
+		expect(actual.queries === queries).toEqual(false);
+	});
+
 
 	it("should SET_QUERY_RESULTS", () => { //eslint-disable-line no-undef
 		expect(queriesReducer(
@@ -28,5 +80,7 @@ describe("queries reducer", () => { //eslint-disable-line no-undef
 			{ some: "state", resultCount: "1", resultCountPending: false }
 		);
 	});
+
+	it("should DELETE_QUERY"); //eslint-disable-line no-undef
 
 });
