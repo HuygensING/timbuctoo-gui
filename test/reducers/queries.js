a2d43c51ecd621d3acc6345fbb67e407b84a4bcd
renevanderark <r.van.der.ark@gmail.com>
2016-03-02 15:33:45 +0100

    adds tests for queries reducer

---
+++ test/reducers/queries.js
@@ -0,0 +1,25 @@
+import expect from "expect";
+import queriesReducer from "../../src/reducers/queries";
+
+describe("queries reducer", () => { //eslint-disable-line no-undef
+
+	it("should SELECT_QUERY"); //eslint-disable-line no-undef
+
+	it("should SET_QUERY_PATH"); //eslint-disable-line no-undef
+
+	it("should SET_QUERY_FIELD_VALUE"); //eslint-disable-line no-undef
+
+	it("should DELETE_QUERY"); //eslint-disable-line no-undef
+
+	it("should SET_QUERY_RESULTS"); //eslint-disable-line no-undef
+
+	it("should SET_QUERY_RESULT_COUNT", () => { //eslint-disable-line no-undef
+		expect(queriesReducer(
+			{ some: "state", resultCountPending: true },
+			{ type: "SET_QUERY_RESULT_COUNT", count: "1" }
+		)).toEqual(
+			{ some: "state", resultCount: "1", resultCountPending: false }
+		);
+	});
+
+});
