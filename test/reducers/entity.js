29b63e6eed4477b0660e968cb431014bd735c3d0
renevanderark <r.van.der.ark@gmail.com>
2016-03-02 14:08:01 +0100

    cleans up entity reducer test

--- test/reducers/entity.js
+++ test/reducers/entity.js
@@ -1,26 +1,14 @@
 import expect from "expect";
-import store from "../../src/store";
 import entityReducer from "../../src/reducers/entity";
 
 describe("entity reducer", () => { //eslint-disable-line no-undef
 
-
-	it("should clear the entity data after SET_VRE", (done) => { //eslint-disable-line no-undef
-		const unsubscribe = store.subscribe(() => {
-			unsubscribe();
-			try {
-				expect(store.getState().entity).toEqual({
-					data: null,
-					domain: null,
-					errorMessage: null
-				});
-				done();
-			} catch(e) {
-				done(e);
-			}
+	it("should clear the entity data after SET_VRE", () => { //eslint-disable-line no-undef
+		expect(entityReducer({data: "x", domain: "y", errorMessage: "z"}, {type: "SET_VRE"})).toEqual({
+			data: null,
+			domain: null,
+			errorMessage: null
 		});
-
-		store.dispatch({type: "SET_VRE", vreId: "WomenWriters"});
 	});
 
 	it("should immutably SET_ENTITY_FIELD_VALUE", () => { //eslint-disable-line no-undef
@@ -28,36 +16,5 @@ describe("entity reducer", () => { //eslint-disable-line no-undef
 		const actual = entityReducer({data: derefData}, {type: "SET_ENTITY_FIELD_VALUE", fieldPath: ["c", 0], value: "f"});
 		expect(actual).toEqual({data: {a: "b", c: ["f", "e"]}});
 		expect(actual.data === derefData).toEqual(false);
-
-
-
-/*		const domain = "dom";
-		const fieldDefinitions = [{name: "a", type: "string"}, {name: "c", type: "multiselect"}];
-
-		let unsubscribe;
-
-
-		unsubscribe = store.subscribe(() => {
-			unsubscribe();
-			const derefData = store.getState().entity.data;
-			unsubscribe = store.subscribe(() => {
-				unsubscribe();
-				try {
-					expect(store.getState().entity).toEqual({
-						data: {a: "", c: ["f"], "@type": "dom"},
-						domain: domain,
-						fieldDefinitions: fieldDefinitions,
-						errorMessage: null
-					});
-					expect(store.getState().entity.data === derefData).toEqual(false);
-					done();
-				} catch(e) {
-					done(e);
-				}
-			});
-			store.dispatch({type: "SET_ENTITY_FIELD_VALUE", fieldPath: ["c", 0], value: "f"});
-		});
-
-		store.dispatch({type: "NEW_ENTITY", domain: domain, fieldDefinitions: fieldDefinitions});*/
 	});
 });
