58f8cd3b047989f6fcb3300300b07acf588af309
renevanderark <r.van.der.ark@gmail.com>
2016-03-08 16:05:26 +0100

    final merge of new-field-defs branch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	apiUrl: {
-		"v2.1": "http://test.repository.huygens.knaw.nl/v2.1",
+		"v2.1": "http://acc.repository.huygens.knaw.nl/v2.1",
 		"v4": "http://localhost:5000"
 	},
 	apiVersion: "v2.1"
