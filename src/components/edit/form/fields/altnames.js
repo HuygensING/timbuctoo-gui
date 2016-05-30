c4ed3942159cf7154cd9495ca9056df07b010867
renevanderark <r.van.der.ark@gmail.com>
2016-05-30 14:27:57 +0200

    [TIM-849] adds altnames form field to gui.

--- src/components/edit/form/fields/altnames.js
+++ src/components/edit/form/fields/altnames.js
@@ -1,16 +1,42 @@
 import React from "react";
 import MultiForm from "hire-forms-multi-form";
+import AltName from "./multiform/altname";
 
 class AltNames extends React.Component {
 
-	render() {
+	onChange(inPath, data) {
+		let path = typeof inPath === "string" ? [inPath] : inPath;
+		this.props.onChange(path, data);
+	}
 
-		return <div><label>{this.props.name}</label></div>;
+	onDelete(path) {
+		let values = this.props.entity.data[this.props.name];
+		values.splice(path[1], 1);
+		this.props.onChange([path[0]], values);
+	}
+
+	render() {
+		console.log("ALTNAMES", this.props.entity.data[this.props.name]);
+
+		return (
+			<div>
+				<label>{this.props.name}</label>
+				<MultiForm
+					attr={this.props.name}
+					component = {AltName}
+					model={{nametype: "", displayName: ""}}
+					onChange={this.onChange.bind(this)}
+					onDelete={this.onDelete.bind(this)}
+					values={this.props.entity.data[this.props.name]} />
+			</div>
+		);
 	}
 }
 
 AltNames.propTypes = {
-	name: React.PropTypes.string
+	entity: React.PropTypes.object,
+	name: React.PropTypes.string,
+	onChange: React.PropTypes.func
 };
 
 export default AltNames;
