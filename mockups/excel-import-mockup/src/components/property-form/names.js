d34225bcbfed45b5217bfb60e5b0f25dc61b5bea
renevanderark <r.van.der.ark@gmail.com>
2016-06-15 12:47:07 +0200

    [TIM-905] adds a little style to names form.

--- mockups/excel-import-mockup/src/components/property-form/names.js
+++ mockups/excel-import-mockup/src/components/property-form/names.js
@@ -28,27 +28,29 @@ class Form extends React.Component {
 		const components = archetype[mappings.collections[collectionData.collection].archetypeName].find((a) => a.name === name).options;
 
 		return (
-			<div style={{margin: "12px"}}>
-				{(propertyMapping.variable || []).map((v, i) => (
-					<span key={i} style={{display: "inline-block", margin: "8px"}}>
-						<div style={{marginBottom: "2px"}}>
-							<a className="pull-right btn-danger btn-xs" onClick={() => this.onRemoveComponent(propertyMapping, i)}>
-								<span className="glyphicon glyphicon-remove" />
-							</a>
-							{v.component}&nbsp;
-						</div>
-						<SelectField onChange={(value) => this.onComponentChange(propertyMapping, i, value)}
-							options={collectionData.variables}
-							placeholder={`Select column for ${v.component}`}
-							value={v.variableName} />
-					</span>
-				))}
-				<span style={{display: "inline-block", margin: "8px"}}>
-					<SelectField onChange={(value) => onSetFieldMapping(collectionData.collection, name, [...(propertyMapping.variable || []), {component: value}])}
-						options={components} placeholder="Add name component..."
-						value={null} />
-				</span>
-			</div>
+			<span>
+				{propertyMapping.variable && propertyMapping.variable.length ? (
+				<div style={{marginBottom: "12px"}}>
+					{(propertyMapping.variable || []).map((v, i) => (
+						<span key={i} style={{display: "inline-block", margin: "8px"}}>
+							<div style={{marginBottom: "2px"}}>
+								<a className="pull-right btn-danger btn-xs" onClick={() => this.onRemoveComponent(propertyMapping, i)}>
+									<span className="glyphicon glyphicon-remove" />
+								</a>
+								{v.component}&nbsp;
+							</div>
+							<SelectField onChange={(value) => this.onComponentChange(propertyMapping, i, value)}
+								options={collectionData.variables}
+								placeholder={`Select column for ${v.component}`}
+								value={v.variableName} />
+						</span>
+					))}
+				</div>) : null}
+
+				<SelectField onChange={(value) => onSetFieldMapping(collectionData.collection, name, [...(propertyMapping.variable || []), {component: value}])}
+					options={components} placeholder="Add name component..."
+					value={null} />
+			</span>
 		);
 	}
 }
