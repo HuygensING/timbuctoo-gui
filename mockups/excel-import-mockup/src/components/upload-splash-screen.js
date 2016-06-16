02875fc04d199d5ccb106eed31f5ae36a08f55c4
renevanderark <r.van.der.ark@gmail.com>
2016-06-16 07:35:09 +0200

    [TIM-905] styles step 1 a litte.

--- mockups/excel-import-mockup/src/components/upload-splash-screen.js
+++ mockups/excel-import-mockup/src/components/upload-splash-screen.js
@@ -7,11 +7,16 @@ class UploadSplashScreen extends React.Component {
 		const { onUpload } = this.props;
 
 		return (
-			<div>
-				<button className="btn btn-lg btn-success" onClick={onUpload}>
+			<div id="splash">
+				<h1>TIMBUCTOO</h1>
+				<p>Get your data stored and connected to the world.<br />
+				Start uploading your data.</p>
+				<button className="btn btn-lg btn-default" onClick={onUpload}>
 					<span className="glyphicon glyphicon-cloud-upload pull-left"></span>
 					&nbsp; Upload
 				</button>
+
+				<p>Need to get started? Here's an example <a>speadsheet.xlsx</a></p>
 			</div>
 		);
 	}
