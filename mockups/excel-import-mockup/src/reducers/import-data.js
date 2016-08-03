0930dfdc803af69a57bd1a10ef67a46bc32155c0
Jauco Noordzij <code@jauco.nl>
2016-08-03 10:41:45 +0200

    clean up sheet adding reducer

--- mockups/excel-import-mockup/src/reducers/import-data.js
+++ mockups/excel-import-mockup/src/reducers/import-data.js
@@ -7,17 +7,37 @@ const initialState = getItem("importData") || {
 	activeCollection: null
 };
 
+function findIndex(arr, f) {
+	let length = arr.length;
+	for (var i = 0; i < length; i++) {
+    if (f(arr[i], i, arr)) {
+      return i;
+    }
+  }
+	return -1;
+}
+
+function sheetRowFromDictToArray(rowdict, arrayOfVariableNames) {
+	return arrayOfVariableNames.map(name => rowdict[name]);
+}
+
+function addRows(curRows, newRows, arrayOfVariableNames) {
+	return curRows.concat(
+		newRows.map(item => sheetRowFromDictToArray(item, arrayOfVariableNames))
+	);
+}
+
 export default function(state=initialState, action) {
 	switch (action.type) {
 		case "START_UPLOAD":
 			return {...state, isUploading: true};
 		case "FINISH_UPLOAD":
 			return {...state,
-				sheets: action.data.collections.map(coll => ({
-					collection: coll.name,
-					variables: coll.variables,
+				sheets: action.data.collections.map(sheet => ({
+					collection: sheet.name,
+					variables: sheet.variables,
 					rows: [],
-					nextUrl: coll.data
+					nextUrl: sheet.data
 				})),
 				activeCollection: action.data.collections[0].name,
 				vre: action.data.vre,
@@ -25,20 +45,19 @@ export default function(state=initialState, action) {
 				executeMappingUrl: action.data.executeMapping
 			};
 		case "COLLECTION_ITEMS_LOADING_SUCCEEDED":
-			var result = {...state};
-			result.sheets = result.sheets.slice();
-			result.sheets
-				.forEach((sheet, i) => {
-					if (sheet.collection === action.collection) {
-						result.sheets[i] = {
-							...sheet,
-							rows: sheet.rows.concat(
-								action.data.items.map(item => sheet.variables.map(name => item[name]))
-							),
-							nextUrl: action.data.next
-						}
-					}
-				});
+			let sheetIdx = findIndex(state.sheets, sheet => sheet.collection === action.collection)
+			var result = {
+				...state,
+				sheets: [
+					...state.sheets.slice(0, sheetIdx),
+					{
+						...state.sheets[sheetIdx],
+						rows: addRows(state.sheets[sheetIdx].rows, action.data.items, state.sheets[sheetIdx].variables),
+						nextUrl: action.data.next
+					},
+					...state.sheets.slice(sheetIdx + 1)
+				]
+			};
 
 			return result;
 		case "COLLECTION_ITEMS_LOADING_FINISHED":
