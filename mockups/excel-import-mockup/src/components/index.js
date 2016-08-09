9aad7cdd736c25b12d1c3a6a55520796bd1218c7
renevanderark <r.van.der.ark@gmail.com>
2016-08-09 11:42:08 +0200

    removes mockups.

--- mockups/excel-import-mockup/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import UploadSplashScreen from "./upload-splash-screen";
-import ArchetypeMappings from "./archetype-mappings";
-import DatasheetMappings from "./datasheet-mappings";
-
-export {UploadSplashScreen, ArchetypeMappings, DatasheetMappings};
