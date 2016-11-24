b3855eacb18ad98d2ff71788bb7ee30142844ae8
renevanderark <r.van.der.ark@gmail.com>
2016-11-24 10:04:34 +0100

    performance optimized rerender of update button during upload.

--- src/actions/upload.js
+++ src/actions/upload.js
@@ -17,8 +17,8 @@ const onUploadFileSelect = (navigateTo, dispatch) => (files, isReupload = false)
       }
       var newPart = req.responseText.substr(pos);
       pos = req.responseText.length;
-      newPart.split("\n").forEach(line => {
-        dispatch({type: "UPLOAD_STATUS_UPDATE", data: line});
+      newPart.split("\n").forEach((line, idx) => {
+        if (idx % 21 === 0) { dispatch({type: "UPLOAD_STATUS_UPDATE", data: line}); }
       });
     };
     req.onload = function () {
