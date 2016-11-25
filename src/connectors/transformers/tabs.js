ca0777cc67cc9075d5bf64deec70ff1c0d5fef98
renevanderark <r.van.der.ark@gmail.com>
2016-11-25 09:58:58 +0100

    fix mappingsAreComplete validator.

--- src/connectors/transformers/tabs.js
+++ src/connectors/transformers/tabs.js
@@ -1,11 +1,14 @@
 import {propertyMappingIsComplete} from "../../validators/property-mappings";
+import {uniq} from "../../util/uniq";
 
 const mappingsAreComplete = (allColumns, ignoredColumns, predicateObjectMappings) => {
   const evaluateColumns = allColumns.filter((col) => ignoredColumns.indexOf(col) < 0);
-  const validMappings = predicateObjectMappings
-    .filter((pom) => propertyMappingIsComplete(pom));
+  const mappedColumns = predicateObjectMappings
+    .filter((pom) => propertyMappingIsComplete(pom))
+    .map((pom) => pom.objectMap.column ? pom.objectMap.column : "TODO")
+    .reduce(uniq, []);
 
-  return evaluateColumns.length <= validMappings.length;
+  return evaluateColumns.length <= mappedColumns.length;
 };
 
 const transformCollectionTabs = (collections, mappings, activeCollection, predicateObjectMappings) =>
