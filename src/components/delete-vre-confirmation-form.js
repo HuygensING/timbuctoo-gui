fbdf6df4440c7b6834b9d1e47a5d7faba450eca0
renevanderark <r.van.der.ark@gmail.com>
2016-12-06 11:09:42 +0100

    fix delete confirm modal.

--- src/components/delete-vre-confirmation-form.js
+++ src/components/delete-vre-confirmation-form.js
@@ -39,7 +39,7 @@ class DeleteVreConfirmationForm extends React.Component {
             <div className="modal-body">
               <p>Are you sure you wish to delete the <strong>{vreId.replace(/^[^_]+_+/, "")}</strong> dataset?</p>
               <p>To confirm, please retype the fully qualified dataset ID in the input below.</p>
-              <p><input type="text" disabled={true} value={vreId} /> </p>
+              <p style={{width: "100%", backgroundColor: "#ddd", overflowX: "auto", whiteSpace: "nowrap"}}>{vreId}</p>
               <p>
                 <input type="text" value={confirmValue} onChange={this.onConfirmInputChange.bind(this)}
                        onKeyPress={this.onKeyPress.bind(this)}
