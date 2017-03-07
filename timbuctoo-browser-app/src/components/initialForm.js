59d05c6061fd0d5eccdb8a66a2e02e40e0f9a813
Jauco Noordzij <code@jauco.nl>
2017-03-07 10:54:07 +0100

    pull url response from react components
     - fixes infinite loop on wrong url
     - makes it easier to find

--- timbuctoo-browser-app/src/components/initialForm.js
+++ timbuctoo-browser-app/src/components/initialForm.js
@@ -1,53 +1,45 @@
 import React, { PropTypes } from "react"
 
-const UrlForm = ({onSampleClick, skip, value, isFetching, errorText,closeErrorDiv }) => {
+const UrlForm = ({onSampleClick, value, isFetching, errorText,closeErrorDiv }) => {
   return (
-
-    skip ?
+    isFetching ?
       <div>
-        <div style={{ zIndex: 3, width: '100%', height: '100%', position: 'absolute', left: '50%', top: '50%' }}><i className="fa fa-spinner fa-spin fa-5x"></i></div>
-        <form className="form" style={{ opacity: 0.5, position: 'relative' }}>{onSampleClick(value)}</form>
-      </div>
-      :
-      
-      isFetching ?
-        <div>
-           {errorText ? 
-            <div style={{ backgroundColor: '#FFCCCC', height: '35px' }} className="container">
-              <div className='text-right' style={{ height: '5px' }}><button type="button" className="btn btn-link btn-sm"><i style={{ color: '#808080' }} onClick={this.props.closeErrorDiv} className="glyphicon glyphicon-remove"></i></button> </div>
-              <div className="container" style={{ color: '#808080' }}><h6>URL not working.</h6></div>
-            </div>
-            :
-             null
-          }
-          <div style={{ zIndex: 3, width: '100%', height: '100%', position: 'absolute', left: '50%', top: '50%' }}><i className="fa fa-spinner fa-spin fa-5x"></i></div>
-          <form className="form" style={{ opacity: 0.5, position: 'relative' }}>
-            <div className="form-group row ">
-              <label>Resource URL: <input className="form-control col-md-12" type="text" id="input_url" defaultValue={value}></input></label>
-            </div>
-            <div className="form-group row">
-              <button className="btn btn-default" onClick={onSampleClick}> Submit </button>
-            </div>
-          </form>
-        </div> :
-        <div>
           {errorText ? 
-            <div style={{ backgroundColor: '#FFCCCC', height: '35px' }} className="container">
-              <div className='text-right' style={{ height: '5px' }}><button type="button" className="btn btn-link btn-sm"><i style={{ color: '#808080' }} onClick={closeErrorDiv} className="glyphicon glyphicon-remove"></i></button> </div>
-              <div className="container" style={{ color: '#808080' }}><h6>The URL you typed is not valid. Please try again.</h6></div>
-            </div>
-            :
-             null
-          }
-          <form className="form" >
-            <div className="form-group row ">
-              <label>Resource URL: <input className="form-control col-md-12" type="text" id="input_url" ></input></label>
-            </div>
-            <div className="form-group row">
-              <button className="btn btn-default" onClick={onSampleClick}> Submit </button>
-            </div>
-          </form>
-        </div>
+          <div style={{ backgroundColor: '#FFCCCC', height: '35px' }} className="container">
+            <div className='text-right' style={{ height: '5px' }}><button type="button" className="btn btn-link btn-sm"><i style={{ color: '#808080' }} onClick={this.props.closeErrorDiv} className="glyphicon glyphicon-remove"></i></button> </div>
+            <div className="container" style={{ color: '#808080' }}><h6>URL not working.</h6></div>
+          </div>
+          :
+            null
+        }
+        <div style={{ zIndex: 3, width: '100%', height: '100%', position: 'absolute', left: '50%', top: '50%' }}><i className="fa fa-spinner fa-spin fa-5x"></i></div>
+        <form className="form" style={{ opacity: 0.5, position: 'relative' }}>
+          <div className="form-group row ">
+            <label>Resource URL: <input className="form-control col-md-12" type="text" id="input_url" defaultValue={value}></input></label>
+          </div>
+          <div className="form-group row">
+            <button className="btn btn-default" onClick={onSampleClick}> Submit </button>
+          </div>
+        </form>
+      </div> :
+      <div>
+        {errorText ? 
+          <div style={{ backgroundColor: '#FFCCCC', height: '35px' }} className="container">
+            <div className='text-right' style={{ height: '5px' }}><button type="button" className="btn btn-link btn-sm"><i style={{ color: '#808080' }} onClick={closeErrorDiv} className="glyphicon glyphicon-remove"></i></button> </div>
+            <div className="container" style={{ color: '#808080' }}><h6>The URL you typed is not valid. Please try again.</h6></div>
+          </div>
+          :
+            null
+        }
+        <form className="form" >
+          <div className="form-group row ">
+            <label>Resource URL: <input className="form-control col-md-12" type="text" id="input_url" ></input></label>
+          </div>
+          <div className="form-group row">
+            <button className="btn btn-default" onClick={onSampleClick}> Submit </button>
+          </div>
+        </form>
+      </div>
   )
 }
 
--- timbuctoo-browser-app/src/router.js
+++ timbuctoo-browser-app/src/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Router, Route, hashHistory} from "react-router";
 import { Provider, connect } from "react-redux";
 import actions from "./actions/actions";
+import {getRdfFromUrl} from "./actions/actions";
 import App from "./components/app";
 import store from "./reducers/store";
 import PropValue from './components/PropValue';
@@ -37,5 +38,19 @@ const router = (
 		</Router>
 	</Provider>
 );
-    
+
+var curUrls = {};
+hashHistory.listen(function({query}) {
+	if (query.url) {
+		var newUrls = query.url;
+		if (!(newUrls instanceof Array)) {
+			newUrls = [newUrls];
+		}
+		if (Object.keys(curUrls).length != newUrls.length || !newUrls.every(url => curUrls.hasOwnProperty(url))) {
+			curUrls = newUrls.reduce(function (agg, cur) { agg[cur] = true; return agg}, {});
+			getRdfFromUrl(newUrls, store.dispatch.bind(store));
+		}
+	}	
+});
+
 export default router;
