f122abda2a48effa311b9e6cc25fd915c68aee0c
vmaccatrozzo <valentina.maccatrozzo@gmail.com>
2017-03-10 13:54:55 +0100

    Fix camel2label function for displaying labels

--- timbuctoo-default-frontend/src/util/camel2label.js
+++ timbuctoo-default-frontend/src/util/camel2label.js
@@ -1,8 +1,17 @@
-export default (camelCase) => 
-  // camelCase ==='birthDate' ? camelCase = "Date of birth":
-  // camelCase ==='deathDate' ? camelCase = "Date of death":
-  
+const labelList = {
+                  "birthDate":"Date of birth", 
+                  "deathDate":"Date of Death",
+                  "forename":"Given name(s) or initial(s)", 
+                  "surname":"Family name(s)", 
+                  "nameLink":"Family name prefix(es) (e.g. ‘de’, ‘van der’, certain noble titles)", 
+                  "genName":"Name suffix(es) (e.g. ‘jr.’, ‘sr.’, ‘Ph.D’)", 
+                  "roleName":"Name prefix(es) (e.g. ‘dr.’, ‘prof.’, royal and certain noble titles)"
+                }
 
+export default (camelCase) => 
+
+  Object.keys(labelList).indexOf(camelCase)>-1 ? camelCase = labelList[camelCase] :
+ 
   camelCase
   .replace(/([A-Z0-9])/g, (match) => ` ${match.toLowerCase()}`)
   .trim()
--- timbuctoo-multi-collection-search/src/components/faceted-search/camel2label.js
+++ timbuctoo-multi-collection-search/src/components/faceted-search/camel2label.js
@@ -1,4 +1,18 @@
-export default (camelCase) => camelCase
+const labelList = {
+                  "birthDate":"Date of birth", 
+                  "deathDate":"Date of Death",
+                  "forename":"Given name(s) or initial(s)", 
+                  "surname":"Family name(s)", 
+                  "nameLink":"Family name prefix(es) (e.g. ‘de’, ‘van der’, certain noble titles)", 
+                  "genName":"Name suffix(es) (e.g. ‘jr.’, ‘sr.’, ‘Ph.D’)", 
+                  "roleName":"Name prefix(es) (e.g. ‘dr.’, ‘prof.’, royal and certain noble titles)"
+                }
+
+export default (camelCase) => 
+
+  Object.keys(labelList).indexOf(camelCase)>-1 ? camelCase = labelList[camelCase] :
+ 
+  camelCase
   .replace(/([A-Z0-9])/g, (match) => ` ${match.toLowerCase()}`)
   .trim()
   .replace(/^./, (match) => match.toUpperCase())
