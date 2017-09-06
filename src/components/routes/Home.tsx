3d6f7b9d671ea67153d3b1844c59ec94f72696ce
Wouter Versluijs <wouterv@oberon.nl>
2017-09-06 17:54:34 +0200

    connected graphql added initial call for schemas

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -7,6 +7,7 @@ import gql from 'graphql-tag';
 import Hero from '../hero/Hero';
 
 interface Props {
+    data?: any;
 }
 
 interface State {
@@ -14,20 +15,28 @@ interface State {
 
 class Home extends Component<Props, State> {
     render () {
+        const { data } = this.props;
         return (
             <section>
                 <FullHelmet pageName="home"/>
                 <Hero search={true} />
+                <pre>{JSON.stringify( data, null, 4)}</pre>
             </section>
         );
     }
 }
 
 const query = gql`
-    query places {
-        clusius_PlacesList {
-            tim_name{value}
-            tim_country{value}
+    query Places {
+        __schema {
+            types {
+                kind,
+                name,
+                possibleTypes {
+                    name
+                    description
+                }
+            }
         }
     }
 `;
