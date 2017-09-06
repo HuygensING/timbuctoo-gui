dd675dfe1106e0319cd2e41b711d9aa508481f08
ian <1990-iss>
2017-09-06 13:05:17 +0200

    // added PrivateRoute

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { ROUTE_PATHS } from '../constants/routeNaming';
+
+const mapStateToProps = (state) => ({
+    loggedIn: state.user.loggedIn
+});
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    const renderRoute = props => (
+        rest.user.loggedIn
+            ? <Component {...props}/>
+            : <Redirect to={{ pathname: ROUTE_PATHS.root, state: {from: props.location}}}/>
+    );
+
+    return <Route render={renderRoute} {...rest} />;
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,4 +1,4 @@
-export interface IAction {
+export interface Action {
   type: string,
   payload: {
       [name: string]: any
