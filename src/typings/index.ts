27819e277149ef5e12fb7b812a6d5b1af0b2ece6
ian <1990-iss>
2017-09-22 10:00:28 +0200

    // omitted __typename to type Component

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,25 +1,28 @@
 import { Permissions } from './permissions';
+import { Component } from './timbuctoo/schema';
 
 export interface KeyValueObject {
     [name: string]: string;
 }
 
 export interface Action {
-    type: string,
+    type: string;
     payload: {
-        [name: string]: any
-    },
-    error: any
+        [name: string]: any;
+    };
+    error: any;
 }
+
+export type ComponentType = Component & { __typename: string };
 
 export interface DataSetProps {
     datasetId: string;
     title: string;
     description: string;
     imageUrl?: string;
-    owner?: any
-    contact?: any
-    provenanceInfo?: any
+    owner?: any;
+    contact?: any;
+    provenanceInfo?: any;
     contributors?: any[];
     licence?: any[];
     roles?: Permissions[];
