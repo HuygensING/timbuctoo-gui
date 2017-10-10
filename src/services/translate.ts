77595b9c96ec50e3b04171d98049f75af61acf27
Jari Zwarts <jarizw@gmail.com>
2017-10-10 17:10:56 +0200

    Fall back to null if translation does not exist. Show warning on dev. Fix incorrect key in en translation file

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -3,10 +3,14 @@ import { store } from '../index';
 
 const DEFAULT_LANGUAGE_KEY = 'en';
 
-export default (translationKey: string) => {
+export default (translationKey: string): string => {
     const languageKey = store.getState().user.language;
     const language = Languages[languageKey];
     const defaultLanguage = Languages[DEFAULT_LANGUAGE_KEY];
+    
+    if (!(translationKey in language) && process.env.NODE_ENV === 'development') {
+        console.error(`Translation key '${translationKey}' does not exist in language '${languageKey}'!`);
+    }
 
-    return language[translationKey] || defaultLanguage[translationKey] || languageKey;
+    return language[translationKey] || defaultLanguage[translationKey] || null;
 };
