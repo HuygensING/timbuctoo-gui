fa57cb2d8b21e90fddef336d3b5aa640cdfeaf2b
ian <1990-iss>
2017-10-25 12:09:41 +0200

    - Changed 'elasticsearch' param. Added extra filter in each 'aggs' to optimize the count we get back - TODO: BE needs a change, so this needs to be checked when that is added

--- src/services/CollectionArgumentsCreator.ts
+++ src/services/CollectionArgumentsCreator.ts
@@ -1,52 +1,129 @@
 import queryString from 'querystring';
 import { FacetConfig, IndexConfig } from '../typings/schema';
-import { decode } from './UrlStringCreator';
+import { EsMatches, EsQuery } from './EsQueryConstructor';
+
+interface Aggs {
+    [name: string]: Agg;
+}
+
+interface Agg {
+    filter?: EsQuery;
+    aggs: {
+        name: {
+            terms: {
+                field: string;
+            }
+        }
+    };
+}
+
+interface ElasticSearchParams {
+    aggs: Aggs;
+    post_filter?: EsQuery;
+}
 
 const doubleStringify = (obj: {}): string => JSON.stringify(JSON.stringify(obj));
 
-const createAggsString = (facets: FacetConfig[]): {} => {
-    const aggs = { aggs: {} };
+/** filter aggregations that match the path out of the search object and return a new instance
+ *
+ * @param {EsQuery} searchObj
+ * @param {string} field
+ * @returns {EsQuery}
+ */
+const setFilteredSearchObj = (searchObj: EsQuery, field: string): EsQuery => {
+    const filteredSearchObj: EsQuery = { bool: { must: [] } };
+
+    searchObj.bool.must.forEach(
+        (obj: EsMatches) => {
+            if (obj.hasOwnProperty('query_string')) {
+                filteredSearchObj.bool.must.push(obj);
+
+            } else if (obj.bool && obj.bool.should && obj.bool.should.length > 0 && obj.bool.should[0].match) {
+                const { match } = obj.bool.should[0];
+                let keyIsField: boolean = false;
+
+                for (let key in match) {
+                    if (match.hasOwnProperty(key)) {
+                        if (key === field) {
+                            keyIsField = true;
+                        }
+                    }
+                }
+
+                if (!keyIsField) {
+                    filteredSearchObj.bool.must.push(obj);
+                }
+            }
+        }
+    );
+
+    return filteredSearchObj;
+};
+
+/** create a string with aggregations. In case of a querystring add a filter for each aggregation to optimize the counts of filters to be shown
+ *
+ * @param {FacetConfig[]} facets
+ * @param {EsQuery} searchObj
+ * @returns {Aggs}
+ */
+const createAggsString = (facets: FacetConfig[], searchObj: EsQuery | null): Aggs => {
+    const aggregations: Aggs = {};
 
     facets.forEach(({ paths, caption, type }: FacetConfig, idx: number) => {
         if (type === 'MultiSelect' && (caption || type) && paths) {
-            aggs.aggs[caption || `${type}_${idx}`] = {
-                terms: {
-                    field: `${paths[0]}.raw`
+            const field = `${paths[0]}.raw`;
+
+            const aggregation: Agg = {
+                aggs: {
+                    name: {
+                        terms: {
+                            field
+                        }
+                    }
                 }
             };
+
+            if (searchObj) {
+                aggregation.filter = setFilteredSearchObj(searchObj, field);
+            }
+
+            aggregations[caption || `${type}_${idx}`] = aggregation;
         }
     });
-    const aggString = doubleStringify(aggs);
-    return aggString.slice(2, -2);
+
+    return aggregations;
 };
 
-const createPostFilterString = (search: string): string => {
-    const searchString = search ? decode(search) : null;
+/** Set the elasticSearch params for the collection. Always returns aggs, in case of a querystring in the url also includes a post_filter
+ *
+ * @param {IndexConfig} indexConfig
+ * @param {string} search
+ * @returns {string}
+ */
+const setElasticSearchParams = (indexConfig: IndexConfig, search: string): string => {
+    const searchObj: EsQuery | null = search ? JSON.parse(search) : null;
+    const aggs: Aggs = createAggsString(indexConfig.facet, searchObj);
 
-    if (searchString && searchString.length > 0) {
-        const searchStr = JSON.stringify(searchString);
-        return `\\"query\\": {${searchStr.slice(2, -2)}}`;
-    }
-    return '';
+    const elasticSearchParams: ElasticSearchParams = !search || !searchObj
+        ? { aggs }
+        : { aggs, post_filter: searchObj };
+
+    return doubleStringify(elasticSearchParams);
 };
 
-const elasticQueryStringCreator = (indexConfig: IndexConfig, search: string): string => {
-    const aggs = createAggsString(indexConfig.facet);
-    const postFilter = createPostFilterString(search);
-    return `elasticsearch: "{${aggs} ${postFilter.length > 0 ? ',' + postFilter : ''}}"`;
-};
-
-const cursorStringCreator = (cursor: string): string => {
-    return cursor ? `, cursor: ${JSON.stringify(cursor)}` : '';
-};
-
+/** Set arguments meant for collections. At the very least returns a stringified elasticsearch query
+ *
+ * @param indexConfig
+ * @param location
+ * @returns {string}
+ */
 const setCollectionArguments = (indexConfig, location): string => {
     const { cursor, search } = queryString.parse(location.search.substring(1));
 
-    const elasticsearch = elasticQueryStringCreator(indexConfig, search);
-    const cursorString = cursorStringCreator(cursor);
+    const elasticsearch = `elasticsearch: ${setElasticSearchParams(indexConfig, search)}`;
+    const cursorString = cursor ? `, cursor: ${doubleStringify(cursor)}` : '';
 
-    return `(${elasticsearch}${cursorString})`;
+    return `(${elasticsearch}${cursorString}`;
 };
 
 export default setCollectionArguments;
