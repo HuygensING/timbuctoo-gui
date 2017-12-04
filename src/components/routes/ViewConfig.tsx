c518d29093f3b5279f42900e02cb94622274141c
ian <1990-iss>
2017-12-04 16:06:35 +0100

    - Removed redundant dependencies

--- src/components/routes/ViewConfig.tsx
+++ src/components/routes/ViewConfig.tsx
@@ -42,8 +42,8 @@ const ViewConfig: SFC<GraphProps> = props => {
         }
 
         return props.mutate!({ variables: { dataSet: dataSetId, collectionUri: collection!.uri, viewConfig } })
-            .then(data => alert(`The collection ${collection!.collectionId} has been updated`)) // TODO: This also should be something fancy
-            .catch(err => console.error('there was an error sending the query', err));
+            .then((data: any) => alert(`The collection ${collection!.collectionId} has been updated`)) // TODO: This also should be something fancy
+            .catch((err: any) => console.error('there was an error sending the query', err));
     };
 
     return (
