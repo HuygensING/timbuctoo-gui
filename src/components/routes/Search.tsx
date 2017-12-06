9008638efff26b3d664cdeba7eba902374f77e8a
ian <1990-iss>
2017-12-06 11:17:13 +0100

    - Removed Filters.tsx entirely

--- src/components/routes/Search.tsx
+++ src/components/routes/Search.tsx
@@ -1,33 +1,43 @@
 import React, { ComponentType, SFC } from 'react';
 import { connect, Dispatch } from 'react-redux';
+import { compose } from 'redux';
+import { ChildProps } from 'react-apollo';
+import { lifecycle, withProps } from 'recompose';
+import { RouteComponentProps, withRouter } from 'react-router';
+
 import { Location } from 'history';
 import { CollectionMetadata, DataSetMetadata, Facet, FacetConfig } from '../../typings/schema';
+
+import metaDataResolver, { MetaDataProps } from '../../services/metaDataResolver';
+import renderLoader from '../../services/renderLoader';
+import graphqlWithProps from '../../services/graphqlWithProps';
+import handleError from '../../services/handleError';
+import { reorderUnknownsInList } from '../../services/HandleUnknowns';
+import { getValuesFromObject } from '../../services/getValue';
+import { getCollectionValues } from '../../services/GetDataSetValues';
+import translate from '../../services/translate';
+import { createEsQueryString } from '../../services/EsQueryStringCreator';
+import { encode } from '../../services/UrlStringCreator';
+
+import QUERY_COLLECTION_PROPERTIES from '../../graphql/queries/CollectionProperties';
+import QUERY_COLLECTION_VALUES from '../../graphql/queries/CollectionValues';
+
 import FullHelmet from '../FullHelmet';
 import { Col, FullSection } from '../layout/Grid';
 import SearchForm from '../form/SearchForm';
 import CollectionTags from '../CollectionTags';
-import { reorderUnknownsInList } from '../../services/HandleUnknowns';
-import Filters from '../Filters';
 import SearchResults from '../search/SearchResults';
-import { getValuesFromObject } from '../../services/getValue';
-import { getCollectionValues } from '../../services/GetDataSetValues';
-import QUERY_COLLECTION_PROPERTIES from '../../graphql/queries/CollectionProperties';
-import QUERY_COLLECTION_VALUES from '../../graphql/queries/CollectionValues';
 import Pagination from '../search/Pagination';
-import metaDataResolver, { MetaDataProps } from '../../services/metaDataResolver';
-import graphqlWithProps from '../../services/graphqlWithProps';
-import { compose } from 'redux';
-import { RouteComponentProps, withRouter } from 'react-router';
-import renderLoader from '../../services/renderLoader';
-import { ChildProps } from 'react-apollo';
-import handleError from '../../services/handleError';
-import { lifecycle, withProps } from 'recompose';
-import { mergeFilters } from '../../reducers/search';
+import { Dummy } from '../Dummy';
+import { Title } from '../layout/StyledCopy';
+import styled, { withProps as withStyledProps } from '../../styled-components';
+import MultiSelectForm from '../form/MultiselectForm';
+
+import { EsFilter, mergeFilters } from '../../reducers/search';
 import { RootState } from '../../reducers/rootReducer';
-import { createEsQueryString } from '../../services/EsQueryStringCreator';
-import { encode } from '../../services/UrlStringCreator';
 
 interface StateProps {
+    filters: EsFilter[];
     callRequested: boolean;
     createQueryString: () => string;
 }
@@ -53,7 +63,12 @@ type ApolloProps = ChildProps<
 
 type FullProps = ApolloProps & StateProps & DispatchProps & ExtraProps;
 
-const Search: SFC<FullProps> = ({ metadata, data, collectionValues }) => {
+// TODO: this is just a simple loading effect, should be way cooler
+const StyledForm = withStyledProps<{ loading: boolean }>(styled.form)`
+    opacity: ${props => (props.loading ? 0.5 : 1)};
+`;
+
+const Search: SFC<FullProps> = ({ metadata, data, collectionValues, filters }) => {
     const { collectionList, dataSetId, collection } = metadata.dataSetMetadata!;
 
     const fields = getValuesFromObject(collection!.summaryProperties);
@@ -79,12 +94,12 @@ const Search: SFC<FullProps> = ({ metadata, data, collectionValues }) => {
 
             <FullSection>
                 <Col sm={12} smPaddingY={1}>
-                    <Filters
-                        loading={data!.loading}
-                        currentCollectionListId={collection!.collectionListId}
-                        collection={collectionValues}
-                        facetConfigs={collection!.indexConfig.facet}
-                    />
+                    <StyledForm onSubmit={e => e.preventDefault()} loading={data!.loading}>
+                        <Title>{translate('globals.filters')}</Title>
+                        <Dummy text={'search-filter'} height={1} marginY={0.5} />
+                        {filters.length > 0 &&
+                            filters.map((filter, idx) => <MultiSelectForm key={idx} filter={filter} index={idx} />)}
+                    </StyledForm>
                 </Col>
 
                 <Col sm={27} smOffset={3} smPaddingY={1}>
@@ -110,6 +125,7 @@ const Search: SFC<FullProps> = ({ metadata, data, collectionValues }) => {
 };
 
 const mapStateToProps = (state: RootState) => ({
+    filters: state.search.filters,
     callRequested: state.search.callRequested,
     createQueryString: () => createEsQueryString(state.search.filters, state.search.fullText)
 });
