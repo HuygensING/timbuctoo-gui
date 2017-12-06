1c7237ba16038ce5159acdb3e8122bd7ab617722
ian <1990-iss>
2017-12-06 11:27:35 +0100

    Fixing type

--- src/services/GetDataSetValues.ts
+++ src/services/GetDataSetValues.ts
@@ -1,5 +1,7 @@
 // TODO: Rename this file!
 
+import { DataSetMetadata } from '../typings/schema';
+
 export function safeGet<T, U extends keyof T>(arr: T | undefined | null, index: U): T[U] | null {
     if (arr) {
         return arr[index];
@@ -8,13 +10,17 @@ export function safeGet<T, U extends keyof T>(arr: T | undefined | null, index: 
     }
 }
 
-const getCollectionValues = (data: any, metadata: any): null => {
+// Casting dataSets as any, for the fields inside are not known to us
+const getCollectionValues = (
+    data: { dataSets?: any } | undefined,
+    metadata: { dataSetMetadata?: DataSetMetadata }
+): null => {
     if (!data || !data.dataSets || !metadata.dataSetMetadata) {
         return null;
     }
 
     const { dataSetId, collection } = metadata.dataSetMetadata;
-    return safeGet(safeGet(data.dataSets, dataSetId), collection.collectionListId);
+    return safeGet(safeGet(data.dataSets, dataSetId), collection!.collectionListId);
 };
 
 export { getCollectionValues };
